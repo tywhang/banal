483aba925b5c4c6ec34236141f21381e2b2a399b
Ryan Burrows <rhburrows@gmail.com>
2015-11-07 12:50:22 -0800

    Call `read_attribute` correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_many :projects, through: :memberships
 
   def name
-    name = read_attribute['name']
+    name = read_attribute('name')
     name.present? ? name : 'Anonymous'
   end
 end
