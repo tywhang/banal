218fe974f1a81d09cb37814ed07c28a4c171efbe
Ryan Burrows <rhburrows@gmail.com>
2015-11-07 13:01:29 -0800

    Add missing devise column
    I accidentally forgot to add this column when setting up devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107204619) do
+ActiveRecord::Schema.define(version: 20151107210010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,9 @@ ActiveRecord::Schema.define(version: 20151107204619) do
   add_index "projects", ["token"], name: "index_projects_on_token", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
-    t.integer  "sign_in_count",      default: 0,  null: false
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
+    t.integer  "sign_in_count",       default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20151107204619) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.datetime "remember_created_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
