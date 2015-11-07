9bb3017294d6c428f4ed799c01034bacc59b7f01
Ryan Burrows <rhburrows@gmail.com>
2015-11-07 13:31:07 -0800

    Auto-assign the demo project to new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,16 @@ class User < ActiveRecord::Base
   has_many :memberships
   has_many :projects, through: :memberships
 
+  after_create :add_demo_membership
+
   def name
     name = read_attribute('name')
     name.present? ? name : 'Anonymous'
+  end
+
+  private
+
+  def add_demo_membership
+    memberships.create(project: Project.demo)
   end
 end
