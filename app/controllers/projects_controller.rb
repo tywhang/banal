cfc802914fa54ab8b64f9c6479f7dc56ddd5975f
Ryan Burrows <rhburrows@gmail.com>
2015-11-07 13:26:36 -0800

    Only let users look at their own projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
     @events = @project.events.order("created_at DESC").first(50)
   end
 
