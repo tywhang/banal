ef080c76ee511eccccad8edd858dde1968ea7772
Ted Whang <ted@quve.com>
2015-11-08 10:59:40 -0800

    Added animation scroll when clicking nav bar links

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -3,4 +3,8 @@ $(function() {
     $(e.target).parents('table').find('.active').removeClass('active');
     $(e.target).parents('.json-row').toggleClass('active');
   });
+
+  $(".js-navigation-link").click(function(e) {
+      $("body").animate({ scrollTop: $($(e.target).data().target).offset().top }, 1000);
+  });
 });
