8a045601810996489883223970e2056b15125171
Woody Peterson <woody.peterson@gmail.com>
2015-11-08 10:53:01 -0800

    Fix ID issue in ES mapping

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,20 @@ class Event < ActiveRecord::Base
   include Elasticsearch::Model::Callbacks
   index_name [Rails.env, model_name.collection.gsub(/\//, '-')].join('_')
 
+  mapping do
+    indexes :actor do
+      indexes :id, type: 'string'
+    end
+
+    indexes :object do
+      indexes :id, type: 'string'
+    end
+
+    indexes :target do
+      indexes :id, type: 'string'
+    end
+  end
+
   belongs_to :project
 
   validates :actor, presence: true
