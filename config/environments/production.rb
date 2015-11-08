9cc9610a54f46824938c72d9fc26bc223b8001c4
Woody Peterson <woody.peterson@gmail.com>
2015-11-08 10:08:26 -0800

    Add production config for elasticsearch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,3 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+Elasticsearch::Model.client =
+  Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
